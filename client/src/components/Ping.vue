c71d2496c569d44e129217dcaf8dda47a12ad484
christianzhou1 <christianzhiruizhou@outlook.com>
2024-09-02 22:55:58 -0400

    integrate vue with backend

--- client/src/components/Ping.vue
+++ client/src/components/Ping.vue
@@ -5,12 +5,30 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Ping",
   data() {
     return {
-      msg: "Hello!",
+      msg: "",
     };
+  },
+  methods: {
+    getMessage() {
+      const path = "http://localhost:5001/ping";
+      axios
+        .get(path)
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+  },
+  created() {
+    this.getMessage();
   },
 };
 </script>
